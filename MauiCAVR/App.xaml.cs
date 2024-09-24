26091bbba91a4d09980dbb0946791c113f7ad226
Jorge Enrique Estrada Correa <proyectosamva@gmail.com>
2024-09-24 15:12:06 -0500

    Sept 24 3:11 pm

---
+++ MauiCAVR/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiCAVR
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiCAVR/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiCAVR
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
